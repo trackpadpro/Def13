662bff0dfe54881cdd4001830b8a31d803c0caa7
Valentin Richter <42276475+trackpadpro@users.noreply.github.com>
2021-11-17 05:26:45 -0500

    Initialization from 2018
    Change variable names and ensure tokens and passwords are hidden before release. Save debug for later.

---
+++ helpme.js
@@ -0,0 +1,5 @@
+module.exports = {
+    instructionManual: function() {
+       return "Commands:\n\"r [subreddit] [sort] [number]\" - search Reddit posts (ex: r prequelmemes hot 3) \n         defaults: subreddit=all, sort=hot, number=1; number cannot exceed 5 \n\"valpal [text]\" - have Def13 echo you";
+    }
+ }
---
+++ imin.js
@@ -0,0 +1,15 @@
+module.exports = {
+    DiscordToken: function() {
+       return 'NDc3MjMyMDgwNDA1NjU5NjQ4.Dk5MNQ.jD-gf_bVGafU8Ef0f7_ztGurYGw';
+    }
+    ,
+    RedditSignIn: function() {
+        return {
+            usergent: 'Discord bot by u/paragon60',
+            userame: 'Def13Bot',
+            passord: '49u7384yt987ur3u81e98uw1i291092ie09iori39',
+            clientd: '0n9E4NMnBmBj2g',
+            clientecret: 'ouRD3IQ9g4wX6i9jAmjlUTkKFV4'
+        };
+    }
+ }
