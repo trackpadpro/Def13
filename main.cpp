b99b1b223cd25e31c0199516390818e342509667
Valentin Richter <valentinsrichter@gmail.com>
2024-02-11 00:52:47 -0500

    add bot status

--- main.cpp
+++ main.cpp
@@ -12,17 +12,31 @@ int main(){
         return 1;
     }
 
-    apiDPP->on_slashcommand([](auto event){
-        if (event.command.get_command_name() == "ping"){
-            event.reply("Pong!");
+    apiDPP->on_log(dpp::utility::cout_logger());
+
+    apiDPP->on_slashcommand([](const dpp::slashcommand_t& event){
+        if(event.command.get_command_name()=="ping"){
+            event.reply("pong");
+        }
+        else if(event.command.get_command_name()=="repo"){
+            event.reply("https://github.com/trackpadpro/Def13");
+        }
+        else if(event.command.get_command_name()=="join"){
+
         }
     });
 
-    apiDPP->on_ready([](auto event){
-        if (dpp::run_once<struct register_bot_commands>()){
-            apiDPP->global_command_create(
-                dpp::slashcommand("ping", "Ping pong!", apiDPP->me.id)
-            );
+    apiDPP->on_ready([](const dpp::ready_t& event){
+        if(dpp::run_once<struct set_status>()){
+            apiDPP->set_presence(dpp::presence(dpp::ps_online, dpp::at_custom, "Lounging"));
+        }
+
+        if(dpp::run_once<struct register_bot_commands>()){
+            apiDPP->global_bulk_command_create({
+                dpp::slashcommand("ping", "ping pong", apiDPP->me.id),
+                dpp::slashcommand("repo", "source code", apiDPP->me.id),
+                dpp::slashcommand("join", "join call", apiDPP->me.id)
+            });
         }
     });
 
